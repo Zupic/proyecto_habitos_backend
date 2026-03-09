29fe069741f1c557c8e1a07dfd5614a23204d0b3
Irma Piche <pushgraff.0k@gmail.com>
2026-03-09 00:14:00 -0600

    third commit Semana 3 - estructura backend

---
+++ clase1/models/Habit.js
@@ -0,0 +1,33 @@
+const mongoose  = require ('mongoose');
+const habitSchema = new mongoose.Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    description: {
+        type: String, 
+        required: true
+    },
+    createdAt:{
+        type:Date,
+        default: Date.now
+    },
+    lastUpdate:{
+        type: Date,
+        default:Date.now
+    },
+    lastDone:{
+        type: Date,
+        default: Date.now
+    },
+    days:{
+        type: Number,
+        default: 1
+    },
+    startedAt:{
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Habit',habitSchema);

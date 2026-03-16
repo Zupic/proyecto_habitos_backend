879fa679a4ec262e4af28d8c5db8b8eae61d0b8f
Irma Piche <pushgraff.0k@gmail.com>
2026-03-16 00:13:47 -0600

    Semana 4 - estructura backend

--- clase1/routes/index.js
+++ clase1/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const Habit = require('../models/Habit');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -31,5 +32,38 @@ router.delete('/habits/:id', async (req,res)=>{
     res.status(500).json({message:'Habit not found'});
   }
 });
+
+//marcar como realizado, primero busca el id, si no han pasado 24h aumenta los días
+router.patch ('/habits/markasdone/:id', async (req,res)=>{
+  try{
+    const habit = await Habit.findById(req.params.id);
+    habit.lastDone = new Date();
+    if(timeDifferenceInHours(habit.lastDone, habit.lastUpdate)<24){
+      habit.days = timeDifferenceInDays(habit.lastDone, habit.startedAt);
+      habit.lastUpdate = new Date();
+      habit.save();
+      res.status(200).json({'message':'Habit marked as done'});
+    }else{
+      habit.days= 0;
+      habit.lastUpdate = new Date();
+      habit.startedAt = new Date();
+      habit.save ();
+      res.status(200).json({'message': 'Habit restarted'});
+    }
+  }catch(err){
+    console.log(err);
+    res.status(500).json({message: 'Habit not found'});
+  }
+});
+//obtiene el valor absoluto
+const timeDifferenceInHours = (date1, date2)=>{
+  const differenceMs = Math.abs(date1 - date2);
+  return differenceMs / (1000 * 60 * 60);
+}
+
+const timeDifferenceInDays = (date1, date2)=>{
+  const differenceMs = Math.abs (date1 - date2);
+  return Math.floor (differenceMs/(1000*60*60*24));
+}
 
 module.exports = router;
